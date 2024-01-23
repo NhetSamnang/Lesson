53b645fbf11a4a393e2aa9a977c4ce7b31458a86
NhetSamnang <nhetsamnang249@gmail.com>
2024-01-23 08:56:31 +0700

    file2

---
+++ AbstractPerson.cs
@@ -0,0 +1,16 @@
+﻿using EntityCollection;
+using System.Windows.Forms;
+
+public abstract class AbstractPerson : Entity<int>
+{
+    private static int count = 0;
+    public AbstractPerson() { Key = ++count; }
+    protected string _name = "";
+    protected string _gender = "";
+    protected byte _age;
+    public string Name { get => _name; set { _name = value; Notify(); } }
+    public string Gender { get => _gender; set { _gender = value; Notify(); } }
+    public byte Age { get => _age; set { _age = value; Notify(); } }
+    public sealed override string ToString()
+    => this.Info; /*Performing polymorphism process */
+}
